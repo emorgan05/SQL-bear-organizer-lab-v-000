b06d283e0c0e9ae379013c53b3d4ad62c443d9a3
Emily Morgan <emorgan05@gmail.com>
2017-10-20 20:38:52 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -5,4 +5,4 @@ INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergea
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 3, "F", "black", "calm", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 27, "M", "brown", "grouchy", 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 10, "F", "brown", "aggressive", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("NULL", 17, "M", "black", "aggressive", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 17, "M", "black", "aggressive", 0);
